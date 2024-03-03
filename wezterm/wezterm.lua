975a34d2d626f42f1118bdccc966285d1545d69f
Guilherme Diniz <guilhermedinizlau@gmail.com>
2024-03-03 10:01:29 -0300

    a

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,24 +1,17 @@
 local wezterm = require 'wezterm'
 return {
-	color_scheme = 'GitHub Dark',
-	enable_tab_bar = false,
-	font_size = 11,
-	font = wezterm.font 'FiraCode Nerd Font Mono',
-	-- font = wezterm.font 'JetBrainsMono Nerd Font Mono',
-	-- font = wezterm.font 'Berkeley Mono Trial',
-	-- font = wezterm.font 'Hack Nerd Font Mono',
-	-- font = wezterm.font 'SpaceMono Nerd Font',
-	-- font = wezterm.font 'SFMono Nerd Font',
-	-- font = wezterm.font 'Martian Mono',
-	macos_window_background_blur = 19,
-	window_decorations = 'NONE',
-	-- window_background_image = '/home/guilherme/.dotfiles/wez_bg.png',
-    window_background_opacity = 1,
-    keys = {
-        {
-            key = 'f',
-            mods = 'CTRL',
-            action = wezterm.action.ToggleFullScreen,
-        },
+  color_scheme = 'GitHub Dark',
+  enable_tab_bar = false,
+  font_size = 11,
+  font = wezterm.font 'JetBrains Mono NL',
+  macos_window_background_blur = 19,
+  window_decorations = 'NONE',
+  window_background_opacity = .95,
+  keys = {
+    {
+      key = 'f',
+      mods = 'CTRL',
+      action = wezterm.action.ToggleFullScreen,
     },
+  },
 }
